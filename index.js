99012056fb59fe10e8ef8682daca576174a51da4
nitishkp3 <140956026+nitishkp3@users.noreply.github.com>
2023-07-31 14:06:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+
+// typing animation
+var typed = new Typed(".typing",{
+    strings: ["","Frontend Developer","Problem Solver","Competitive Programmer","Web Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+// const navi = document.querySelectorAll(".navi");
+
+// function switchTab(clickedTab){
+//     Arrays.forEach(element =>{
+//         if(element==clickedTab){
+//             element.classList.add("active");
+//         }
+//         else{
+//             element.classList.remove("active");
+//         }
+//     })
+// }
+
+// navi.addEventListener("click", () => {
+//     switchTab(navi);
+// });
+
+
+// window.addEventListener("click",(event) => {
+//     navi.forEach(element => {
+//         if(event.target === element){
+//             navi.classList.add("active");
+//         }
+//         else{
+//             navi.classList.remove("active");
+//         }
+//     });
+// })
+
+
